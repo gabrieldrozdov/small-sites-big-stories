f5806cf7d518e722cb481b4ba4b2801a0b4d516c
Gabriel Drozdov <gabrieldrozdov@gmail.com>
2026-02-27 09:14:17 -0500

    new editor page

--- stories.js
+++ stories.js
@@ -1,15 +1,100 @@
+// Fetch overview data and check if story in URL exists
+// If not, load random story
+let activeStory;
+let storyData;
+let storyNames;
+async function initStory() {
+	const pageHref = window.location.search;
+	const searchParams = new URLSearchParams(pageHref.substring(pageHref.indexOf('?')));
+	if (searchParams.has('story')) {
+		activeStory = searchParams.get('story');
+	} else {
+		activeStory = '_NO_ACTIVE_STORY_';
+	}
+	try {
+		let response = await fetch(`../stories.json`);
+		response.json().then((json) => {
+			storyData = json;
+			storyNames = Object.keys(storyData);
+			if (!storyNames.includes(activeStory)) {
+				activeStory = storyNames[Math.floor(Math.random()*storyNames.length)];
+			}
+			fetchStory();
+		});
+	}
+	catch(e) {
+		alert("Something went wrong while trying to load your story! Try checking your Internet connection and refreshing the page.");
+	}
+}
+initStory();
+
+let activeStoryData;
+let activeStoryHTML;
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+async function fetchStory() {
+	// Populate header
+	activeStoryData = storyData[activeStory];
+
+	const headerInfoTitle = document.querySelector(`.header-info-title`);
+	headerInfoTitle.innerHTML = activeStoryData['title'];
+
+	const headerInfoDetails = document.querySelector(`.header-info-details`);
+	const date = activeStoryData['date'];
+	headerInfoDetails.innerHTML = `<li>Published ${months[date[0]-1]} ${date[1]}, ${date[2]}</li>`;
+	for (let detail of activeStoryData['details']) {
+		headerInfoDetails.innerHTML += `<li>${detail}</li>`;
+	}
+
+	const headerDesc = document.querySelector(`.header-desc`);
+	headerDesc.innerHTML = activeStoryData['desc'];
+
+	let prevStory, nextStory;
+	let activeStoryIndex = storyNames.indexOf(activeStory);
+
+	if (activeStoryIndex == 0) {
+		prevStory = storyNames[storyNames.length-1];
+	} else {
+		prevStory = storyNames[activeStoryIndex-1];
+	}
+	const navPrev = document.querySelector('#nav-prev');
+	navPrev.href = `./?story=${prevStory}`;
+
+	if (activeStoryIndex == storyNames.length-1) {
+		nextStory = storyNames[0];
+	} else {
+		nextStory = storyNames[activeStoryIndex+1];
+	}
+	const navNext = document.querySelector('#nav-next');
+	navNext.href = `./?story=${nextStory}`;
+
+	const newtab = document.querySelector("#newtab");
+	newtab.href = `${activeStory}.html`;
+
+	// Fetch story data
+	try {
+		let response = await fetch(`./${activeStory}.html`);
+		response.text().then((text) => {
+			activeStoryHTML = text;
+			generateEditor();
+		});
+	}
+	catch(e) {
+		alert("Something went wrong while trying to load your story! Try checking your Internet connection and refreshing the page.");
+	}
+}
+
 let cm;
 let update;
 let updateCount = 0;
 let wrapping = false;
 let fullscreen = false;
 let themes = [ 'smallsites-bw', 'smallsites-light', 'smallsites-dark'];
-let themeIndex = 1;
+let themeIndex = 0;
 function generateEditor() {	
 	const codeContainer = document.querySelector('.code-cm');
 	cm = CodeMirror(codeContainer, {
 		mode: "htmlmixed",
-		value: "<h1>Hello world!</h1>",
+		value: activeStoryHTML,
 		autoCloseTags: false,
 		autoCloseBrackets: false,
 		matchBrackets: true,
@@ -25,7 +110,7 @@ function generateEditor() {
 			'Ctrl-/': 'toggleComment',
 		},
 		lineWrapping: false,
-		theme: "smallsites-light",
+		theme: "smallsites-bw",
 	});
 	cm.on("change", updatePreview);
 
@@ -41,7 +126,6 @@ function generateEditor() {
 
 	updatePreview();
 }
-generateEditor();
 
 // Update preview when changes made in editor
 function updatePreview() {
@@ -80,6 +164,13 @@ function editorToggleWrapping() {
 	cm.setOption('lineWrapping', wrapping);
 }
 
+// Fullscreen
+function editorToggleFullscreen() {
+	fullscreen = !fullscreen;
+	const container = document.querySelector('.container');
+	container.dataset.fullscreen = fullscreen;
+}
+
 // Reset code
 function editorReset() {
 	window.location.reload();
@@ -96,7 +187,7 @@ function editorDownload() {
 	blobURL = URL.createObjectURL(codeBlob);
 	let tempLink = document.createElement("a");
 	tempLink.href = blobURL;
-	tempLink.download = "my-story";
+	tempLink.download = activeStory;
 	tempLink.click();
 }
 
